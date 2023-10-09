7a5d950502ace7c2b0153ec6c9014f283c0802f3
KarenGomes <karenecris12015@gmail.com>
2023-10-09 16:30:22 -0300

    aula 3

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import fs from 'fs';
 import chalk from "chalk"; //no .json tem que colocar abaixo do main "type": "module"
 
-const textoTeste = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)."
-
 function extraiLink(texto){
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = regex.exec(texto);
-    console.log(capturas);
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}))
+    return resultados; 
 }
 
-extraiLink(textoTeste);
 
 function trataErro(erro){
     console.log(erro); 
@@ -21,9 +19,10 @@ async function pegaArquivo(caminhoDoArquivo){
     try {    
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding) 
-        console.log(chalk.green(texto));
+        console.log(extraiLink(texto));
     }catch(erro){
         trataErro(erro)
     }
 }
-//pegaArquivo('./arquivos/texto.md');
+
+pegaArquivo('./arquivos/texto.md');
